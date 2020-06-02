747c4001187f47d37ec69603b2022b49d8bb0001
Philippe Lamarre <philippe.lamarre@radio-canada.ca>
2020-06-02 16:37:29 -0400

    album

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = "DJ while()";
   songs = [
     {id: 1, name:'Missing you'},
-    {id: 2, name:'Batman'},
+    {id: 2, name:'The way'},
     {id: 5, name:'BatGirl'},
     {id: 3, name:'Robin'},
     {id: 4, name:'Flash'}
