f8c565fc71f49a016a17076e636063390dbc6aa8
Philippe Lamarre <philippe.lamarre@radio-canada.ca>
2020-06-02 16:31:37 -0400

    setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = "DJ while()";
+  songs = [
+    {id: 1, name:'Missing you'},
+    {id: 2, name:'Batman'},
+    {id: 5, name:'BatGirl'},
+    {id: 3, name:'Robin'},
+    {id: 4, name:'Flash'}
+];
+}
